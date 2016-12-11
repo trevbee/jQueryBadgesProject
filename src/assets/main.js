f34564f64fc60c1ff3f60267178f4b57a5321478
trevor borthwick <trevor.borthwick@gmail.com>
2016-12-10 21:36:14 -0500

    Project completed

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,43 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/trevbee.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+	    // console.log('response', response.courses.completed);
+      addCourses(response.courses.completed);
+    }
+  });
 
+  function addCourses(courses) {
+    var $badges = $('#badges');
+
+    courses.forEach(function(course) {
+
+//console.log(course);
+
+    var $course =  $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($course);
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo($course);
+
+      $('<a />', {
+        'class': 'btn btn-primary',
+        target: '_blank',
+        href:course.url,
+        text: 'See Course'
+      }).appendTo($course);
+
+    })
+
+  }
 });
